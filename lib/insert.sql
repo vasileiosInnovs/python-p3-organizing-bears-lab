b01f0ee796bf7d3bbe72dfebeeb60b41d89b97a1
Hagee <hagee.ktr@outlook.com>
2025-05-19 14:16:41 +0300

    Select bears based on criteria

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'F', 'brown', 'moody', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'brown', 'moody', 1);
 INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 6, 'M', 'black', 'rowdy', 1);
 INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'white', 'calm', 1);
 INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'M', 'brown', 'furious', 1);
